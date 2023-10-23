c99da8a44cc77ddcfcc23e99e7776d6c658089e8
Krooshk <kirill.krush@mail.ru>
2023-10-23 13:59:37 +0300

    copy given new-catalog-headers

--- src/modules/productDetail/productDetail.ts
+++ src/modules/productDetail/productDetail.ts
@@ -4,79 +4,67 @@ import { formatPrice } from '../../utils/helpers';
 import { ProductData } from 'types';
 import html from './productDetail.tpl.html';
 import { cartService } from '../../services/cart.service';
-import { favoriteService } from '../../services/favorite.service';
-// import localforage from 'localforage';
 
 class ProductDetail extends Component {
-	more: ProductList;
-	product?: ProductData;
+  more: ProductList;
+  product?: ProductData;
 
-	constructor(props: any) {
-		super(props);
+  constructor(props: any) {
+    super(props);
 
-		this.more = new ProductList();
-		this.more.attach(this.view.more);
-	}
+    this.more = new ProductList();
+    this.more.attach(this.view.more);
+  }
 
-	async render() {
-		const urlParams = new URLSearchParams(window.location.search);
-		const productId = Number(urlParams.get('id'));
+  async render() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = Number(urlParams.get('id'));
 
-		const productResp = await fetch(`/api/getProduct?id=${productId}`);
-		this.product = await productResp.json();
+    const productResp = await fetch(`/api/getProduct?id=${productId}`);
+    this.product = await productResp.json();
 
-		if (!this.product) return;
+    if (!this.product) return;
 
-		const { id, src, name, description, salePriceU } = this.product;
+    const { id, src, name, description, salePriceU } = this.product;
 
-		this.view.photo.setAttribute('src', src);
-		this.view.title.innerText = name;
-		this.view.description.innerText = description;
-		this.view.price.innerText = formatPrice(salePriceU);
-		this.view.btnBuy.onclick = this._addToCart.bind(this);
-		this.view.btnFavorite.onclick = this._addToFavorite.bind(this);
+    this.view.photo.setAttribute('src', src);
+    this.view.title.innerText = name;
+    this.view.description.innerText = description;
+    this.view.price.innerText = formatPrice(salePriceU);
+    this.view.btnBuy.onclick = this._addToCart.bind(this);
 
-		const isInCart = await cartService.isInCart(this.product);
-		const isInFavorite = await favoriteService.isInFavorite(this.product);
-		if (isInCart) this._setInCart();
-		if (isInFavorite) this._setInFavorite();
+    const isInCart = await cartService.isInCart(this.product);
 
-		fetch(`/api/getProductSecretKey?id=${id}`)
-			.then((res) => res.json())
-			.then((secretKey) => {
-				this.view.secretKey.setAttribute('content', secretKey);
-			});
+    if (isInCart) this._setInCart();
 
-		fetch('/api/getPopularProducts')
-			.then((res) => res.json())
-			.then((products) => {
-				this.more.update(products);
-			});
-	}
+    fetch(`/api/getProductSecretKey?id=${id}`)
+      .then((res) => res.json())
+      .then((secretKey) => {
+        this.view.secretKey.setAttribute('content', secretKey);
+      });
 
-	private _addToCart() {
-		if (!this.product) return;
+    fetch('/api/getPopularProducts', {
+        headers: {
+          'x-userid': window.userId
+        }
+      })
+      .then((res) => res.json())
+      .then((products) => {
+        this.more.update(products);
+      });
+  }
 
-		cartService.addProduct(this.product);
-		this._setInCart();
-	}
+  private _addToCart() {
+    if (!this.product) return;
 
-	private _addToFavorite() {
-		if (!this.product) return;
-		favoriteService.addProduct(this.product);
-		this._setInFavorite();
-	}
+    cartService.addProduct(this.product);
+    this._setInCart();
+  }
 
-	private _setInCart() {
-		this.view.btnBuy.innerText = '✓ В корзине';
-		this.view.btnBuy.disabled = true;
-	}
-
-	private _setInFavorite() {
-		this.view.btnFavorite.querySelector('use').setAttribute('xlink:href', '#heart_full')
-		this.view.btnFavorite.disabled = true;
-		// localforage.clear();
-	}
+  private _setInCart() {
+    this.view.btnBuy.innerText = '✓ В корзине';
+    this.view.btnBuy.disabled = true;
+  }
 }
 
 export const productDetailComp = new ProductDetail(html);
